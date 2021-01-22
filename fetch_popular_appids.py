498fb366f8efff2b69a86cfe0646036359b0b77e
Wok <woctezuma@users.noreply.github.com>
2021-01-22 19:35:10 +0100

    Export data as a JSON for reuse in a web-app
    https://github.com/woctezuma/heroku-levenshtein-api/

--- fetch_popular_appids.py
+++ fetch_popular_appids.py
@@ -1,6 +1,6 @@
-from disk_utils import save_app_ids
+from disk_utils import save_app_ids, save_json, get_data_folder_name
 from download_utils import get_endpoints, fetch_from_every_endpoint
-from fetch_snapshot_appids import load_snapshot_app_ids
+from fetch_snapshot_appids import load_snapshot_app_ids, load_snapshot
 
 
 def aggregate_top_app_ids(endpoints, rankings, num_apps=3000, save_to_disk=False):
@@ -37,6 +37,18 @@ def load_aggregated_app_ids():
     return popular_app_ids
 
 
+def export_data_for_webapp(intersected_app_ids):
+    snapshot_data = load_snapshot()
+    apps = snapshot_data["response"]["apps"]
+
+    exported_data = []
+    for app in apps:
+        if int(app["appid"]) in intersected_app_ids:
+            exported_data.append({"id": app["appid"], "name": app["name"]})
+
+    return exported_data
+
+
 def main():
     popular_app_ids = load_aggregated_app_ids()
 
@@ -46,6 +58,10 @@ def main():
     print("After intersection: {} apps".format(len(intersected_app_ids)))
 
     save_app_ids(list(intersected_app_ids), "intersected_app_ids")
+
+    exported_data = export_data_for_webapp(intersected_app_ids)
+    print("To export: {} apps".format(len(exported_data)))
+    save_json(exported_data, fname=get_data_folder_name() + "game-list.json")
 
     return
 
